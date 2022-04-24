40303d7eb2a7062a438f04b9ce8593f6dd6d084d
Andres Beltran <anjubelquin@gmail.com>
2022-04-24 13:47:34 -0500

    Finished Percentage Calculator

--- javascript/Cal.js
+++ javascript/Cal.js
@@ -1,36 +1,20 @@
-// var name = "Andres";
-// var age = 20;
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+var form = document.getElementById('xIsWhatPercentOfY');
 
-// var message = `Hi, my name is ${name} and I am ${age} years old!`;
+form.addEventListener('submit', function(event) {
+    if (!numField1.value || !numField2.value) {
+        alert("Please enter values in the field");
+    } else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
 
-// var firstName = "David";
-// var lastName = "Beltran";
-// var dateOfBirth = "04-16-2002";
-// var age = "20";
-// var profileImgUrl = "";
-// var loginWelcomeMessage = `Welcome, ${firstName}. Happy ${age}th birthday!`;
+        var result = x / y;
+        var percent = result * 100;
 
-// console.log(loginWelcomeMessage);
+        resultField.innerText = `Answer: ${percent} %`;
+        event.preventDefault();
+    }
 
-
-// var sum = 10 + 15;
-// var sub = 25 - 55;
-// var mult = 10 * 10;
-// var div = 10 / 3;
-// var mod = 10 % 3;
-
-// var msg = `10 / 3 = 3 with remainder of ${mod}`;
-
-// var results = 10 + 5 * 4 / 6 - 7;
-// console.log(results);
-
-
-function area(length, width) {
-    return length * width;
-}
-
-var rectangleAreas = []
-rectangleAreas.push(area(10,15), area(14,2), area(4,5));
-console.log(rectangleAreas);
-
-
+});
