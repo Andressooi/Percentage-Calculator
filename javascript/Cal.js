c2802f2a9d9a4f872a69dd3ab3ac4018839c40f6
Andres Beltran <anjubelquin@gmail.com>
2022-04-24 12:35:46 -0500

    First Commit

---
+++ javascript/Cal.js
@@ -0,0 +1,36 @@
+// var name = "Andres";
+// var age = 20;
+
+// var message = `Hi, my name is ${name} and I am ${age} years old!`;
+
+// var firstName = "David";
+// var lastName = "Beltran";
+// var dateOfBirth = "04-16-2002";
+// var age = "20";
+// var profileImgUrl = "";
+// var loginWelcomeMessage = `Welcome, ${firstName}. Happy ${age}th birthday!`;
+
+// console.log(loginWelcomeMessage);
+
+
+// var sum = 10 + 15;
+// var sub = 25 - 55;
+// var mult = 10 * 10;
+// var div = 10 / 3;
+// var mod = 10 % 3;
+
+// var msg = `10 / 3 = 3 with remainder of ${mod}`;
+
+// var results = 10 + 5 * 4 / 6 - 7;
+// console.log(results);
+
+
+function area(length, width) {
+    return length * width;
+}
+
+var rectangleAreas = []
+rectangleAreas.push(area(10,15), area(14,2), area(4,5));
+console.log(rectangleAreas);
+
+
